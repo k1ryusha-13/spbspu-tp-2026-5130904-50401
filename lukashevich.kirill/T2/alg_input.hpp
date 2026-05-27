d2d7373df9308a9a98cebb64d57ac169a8d63f55
kirusha-13 <kirillukashevich2007@gmail.com>
2026-05-27 17:34:27 +0300

    remake structures

--- lukashevich.kirill/T2/alg_input.hpp
+++ lukashevich.kirill/T2/alg_input.hpp
@@ -3,18 +3,68 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 namespace lukashevich {
+  struct DblSci
+  {
+    double value;
+  };
+
+  struct UllBin
+  {
+    unsigned long long value;
+  };
+
   struct DataStruct
   {
-    double key1;
-    unsigned long long key2;
+    DblSci key1;
+    UllBin key2;
     std::string key3;
   };
 
-  bool compareData(const DataStruct &left, const DataStruct &right);
+  struct Delimiter
+  {
+    char expected;
+  };
+
+  struct KeyValueInput
+  {
+    std::string key;
+    std::vector< bool > &usedKeys;
+    DataStruct &data;
+  };
+
+  class IOGuard
+  {
+  public:
+    explicit IOGuard(std::basic_ios< char > &stream);
+    ~IOGuard();
+
+  private:
+    std::basic_ios< char > &stream_;
+    std::streamsize precision_;
+    std::streamsize width_;
+    std::basic_ios< char >::fmtflags flags_;
+    char fill_;
+  };
+
+  std::istream &operator>>(std::istream &in, DblSci &data);
+  std::ostream &operator<<(std::ostream &out, const DblSci &data);
+  bool operator<(const DblSci &left, const DblSci &right);
+  bool operator==(const DblSci &left, const DblSci &right);
+
+  std::istream &operator>>(std::istream &in, UllBin &data);
+  std::ostream &operator<<(std::ostream &out, const UllBin &data);
+  bool operator<(const UllBin &left, const UllBin &right);
+  bool operator==(const UllBin &left, const UllBin &right);
+
+  std::istream &operator>>(std::istream &in, Delimiter &&data);
+  std::istream &operator>>(std::istream &in, KeyValueInput &&data);
+
   std::istream &operator>>(std::istream &in, DataStruct &data);
   std::ostream &operator<<(std::ostream &out, const DataStruct &data);
+  bool operator<(const DataStruct &left, const DataStruct &right);
 }
 
 #endif
